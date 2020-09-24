dcc8523111d2c4d29fdea180ae12af71447406fa
Kyle Teel <38758663+Infinite-Zero@users.noreply.github.com>
2020-09-24 17:20:30 -0500

    Update COCOMO_python.py
    swapped dimension of array ([3][4] -> [4][3])
    code works, formatting is different from c++

--- COCOMO_python.py
+++ COCOMO_python.py
@@ -1,5 +1,5 @@
 def main():
-    table = [[2.4,1.05,2.5],[0.38,3.0,1.12],[2.5,0.35,3.6],[1.20,2.5,0.32] ]
+    table = [[2.4,1.05,2.5,0.38],[3.0,1.12,2.5,0.35],[3.6,1.20,2.5,0.32]]
     mode = ["Organic","Semi-Detached","Embedded"]
     size = 4
     calculate(table, 3, mode, size)
